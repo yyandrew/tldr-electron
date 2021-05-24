f19e98babc68aecf904ee37253e0a0643b8b921a
yyandrew <qqerqqer@126.com>
2021-05-24 22:09:01 +0800

    init app

---
+++ src/react.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+export const Index = () => <h2>Hello React!</h2>
+
+ReactDOM.render(<Index />, document.querySelector('#app'))
